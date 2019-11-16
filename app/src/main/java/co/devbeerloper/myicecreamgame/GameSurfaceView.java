2b190133c899f09789a5492b76f79c86183b6417
chechoXR <47802275+chechoXR@users.noreply.github.com>
2019-11-16 17:35:43 -0500

    Tamaño de carro de helados cambiado y nubes dinámicas

--- app/src/main/java/co/devbeerloper/myicecreamgame/GameSurfaceView.java
+++ app/src/main/java/co/devbeerloper/myicecreamgame/GameSurfaceView.java
@@ -7,26 +7,46 @@ import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameSurfaceView extends SurfaceView implements Runnable {
 
     private boolean isPlaying;
     private IceCreamCar icecreamCar;
+
+    private ArrayList<Cloud> clouds;
+    ArrayList<Integer> removeID;
     private Paint paint;
+    private Paint paintCloud;
     private Canvas canvas;
     private SurfaceHolder holder;
     private Thread gameplayThread = null;
+    private Random rd;
+    private Context context;
+    private float screenWith;
+    private float screenHeight;
 
     /**
      * Contructor
+     *
      * @param context
      */
     public GameSurfaceView(Context context, float screenWith, float screenHeight) {
         super(context);
         icecreamCar = new IceCreamCar(context, screenWith, screenHeight);
+
+        this.clouds = new ArrayList<Cloud>();
         paint = new Paint();
+        paintCloud = new Paint();
         holder = getHolder();
         isPlaying = true;
+        rd = new Random();
+        this.context = context;
+        this.screenWith = screenWith;
+        this.screenHeight = screenHeight;
     }
 
     /**
@@ -43,15 +63,50 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
     }
 
     private void updateInfo() {
-        icecreamCar.updateInfo ();
+        icecreamCar.updateInfo();
+
+        for (Cloud c : clouds) {
+            c.updateInfo();
+        }
+
     }
 
     private void paintFrame() {
-        if (holder.getSurface().isValid()){
+        if (holder.getSurface().isValid()) {
             canvas = holder.lockCanvas();
-            canvas.drawColor(Color.CYAN);
-            canvas.drawBitmap(icecreamCar.getSpriteIcecreamCar(),icecreamCar.getPositionX(),icecreamCar.getPositionY(),paint);
-            holder.unlockCanvasAndPost(canvas);
+            canvas.drawColor(Color.rgb(52, 153, 255));
+
+
+            //holder.unlockCanvasAndPost(canvas);
+            removeID = new ArrayList<Integer>();
+
+
+            for (int i=0; i< clouds.size(); i++) {
+             if(clouds.get(i).isVisible())
+                canvas.drawBitmap(clouds.get(i).getSpriteIcecreamCar(), clouds.get(i).getPositionX(), clouds.get(i).getPositionY(), new Paint());
+             else
+                removeID.add(i);
+            }
+
+            for (int i=0; i< removeID.size(); i++)
+                clouds.remove(removeID.get(i));
+
+            removeID.clear();
+
+
+            canvas.drawBitmap(icecreamCar.getSpriteIcecreamCar(), icecreamCar.getPositionX(), icecreamCar.getPositionY(), paint);
+            int r = rd.nextInt(100);
+            if (r > 95) {
+                Cloud cloud = new Cloud(getContext(), screenWith, screenHeight);
+                //canvas.drawBitmap(cloud.getSpriteIcecreamCar(),cloud.getPositionX(),cloud.getPositionY(),paintCloud);
+                clouds.add(cloud);
+                canvas.drawBitmap(cloud.getSpriteIcecreamCar(), cloud.getPositionX(), cloud.getPositionY(), new Paint());
+                holder.unlockCanvasAndPost(canvas);
+            }
+            else
+                holder.unlockCanvasAndPost(canvas);
+
+
         }
 
     }
@@ -67,7 +122,6 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
     }
 
 
-
     public void resume() {
 
         isPlaying = true;
@@ -77,6 +131,7 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
 
     /**
      * Detect the action of the touch event
+     *
      * @param motionEvent
      * @return
      */
